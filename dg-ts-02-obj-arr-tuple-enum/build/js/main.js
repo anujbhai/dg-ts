eac24ae129c3ba1f41a309712eb22cece1c4d6da
anuj <anuj@C40LAP-10090.india.rsystems.com>
2023-03-29 23:54:01 +0530

    dg-ts03-obj-arr-tuple-enum - arr and tuple done.

--- dg-ts-02-obj-arr-tuple-enum/build/js/main.js
+++ dg-ts-02-obj-arr-tuple-enum/build/js/main.js
@@ -1,3 +1,12 @@
 "use strict";
-let username = "Shahjahan";
-console.log(username);
+let str_arr = ["one", "hey", "Anuj"];
+let guitars = ["Tele", "Les Paul", "Casino"];
+let mixed_data = ["EVH", 1984, true];
+let test = [];
+let bands = [
+    "Slipknot",
+    "Slayer"
+];
+bands.push("Thin Lizzy");
+// tuple
+let my_tuple = ["Dave", 42, true];
--- dg-ts-02-obj-arr-tuple-enum/src/main.ts
+++ dg-ts-02-obj-arr-tuple-enum/src/main.ts
@@ -1,4 +1,14 @@
-let username = "Shahjahan";
+let str_arr: string[] = ["one", "hey", "Anuj"];
+let guitars: string[] = ["Tele", "Les Paul", "Casino"];
+let mixed_data: (string | number | boolean)[] = ["EVH", 1984, true];
+let test: any[] = [];
+let bands: string[] = [
+  "Slipknot",
+  "Slayer"
+];
+bands.push("Thin Lizzy");
 
-console.log(username);
+// tuple
+let my_tuple: [string, number, boolean] = ["Dave", 42, true];
+
 

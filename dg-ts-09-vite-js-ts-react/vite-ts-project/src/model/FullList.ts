3e241a255cabe5f3f99239c12f73dc427f42a726
anuj <anuj@C40LAP-10090.india.rsystems.com>
2023-04-09 23:27:48 +0530

    dg-ts10a-project-from-scratch - template class.

--- dg-ts-09-vite-js-ts-react/vite-ts-project/src/model/FullList.ts
+++ dg-ts-09-vite-js-ts-react/vite-ts-project/src/model/FullList.ts
@@ -18,6 +18,20 @@ export default class FullList implements List {
     return this._list;
   }
 
+  load(): void {
+      const stored_list: string | null = localStorage.getItem("my_list");
+
+      if (typeof stored_list !== "string") return;
+
+      const parsed_list: {_id: string, _item: string, _checked: boolean}[] = JSON.parse(stored_list);
+
+      parsed_list.forEach(itemObj => {
+        const new_item_list = new ListItem(itemObj._id, itemObj._item, itemObj._checked);
+
+        FullList.instance.addItem(new_item_list);
+      });
+  }
+
   save(): void {
     localStorage.setItem("my_list", JSON.stringify(this._list));
   }
@@ -27,6 +41,14 @@ export default class FullList implements List {
       this.save();
   }
 
+  addItem(itemObj: ListItem): void {
+      this._list.push(itemObj);
+      this.save();
+  }
 
+  removeItem(id: string): void {
+      this._list = this._list.filter(item => item.id !== id);
+      this.save();
+  }
 }
 
